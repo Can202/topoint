04455e95d32226b1da27d895716f0f24dee466e7
Can202 <mgoopazo@gmail.com>
2023-07-08 16:15:07 -0400

    functions, and the main one

--- src/tptc/tptc.cpp
+++ src/tptc/tptc.cpp
@@ -8,8 +8,15 @@ int transform_code_to_cpp(string program_location, string output);
 void write_text(string dir, string text);
 string get_text_from_file(string dir);
 vector<string> split(const string &s, char delim);
-bool check_if_string_is_in_string(string s1, string s2);
-
+bool str_in(string s1, string s2);
+void get_fuctions(vector<string> textsplit);
+string ident(int num);
+void read_function(vector<string> textsplit, string function_name);
+int when_function_ends(vector<string> textsplit, int function_position);
+string rm_prefix(string full_string, string prefix);
+string rm_suffix(string full_string, string suffix);
+bool str_starts_with(string full_string, string prefix);
+vector<string> removeidentation (vector<string> textsplit);
 
 vector<string> normal_includes = {	"#include <iostream>",
 									"#include <fstream>",
@@ -17,6 +24,13 @@ vector<string> normal_includes = {	"#include <iostream>",
 									"#include <vector>",
 									"#include <string>"
 									};
+int func_start_position = 0;
+vector<string> custom_functions;
+vector<string> custom_functions_type;
+vector<string> custom_functions_arg;
+vector<int> custom_functions_position;
+
+vector<string> variable_types = {"int", "float", "number", "string", "bool"};
 
 int main (int argc, char* argv[]){
 	
@@ -38,6 +52,9 @@ int transform_code_to_cpp(string program_location, string output){
 	string text = get_text_from_file(program_location);
 	
 	vector<string> textsplit = split(text, '\n');
+	
+	textsplit = removeidentation (textsplit);
+	
 	int size = textsplit.size();
 	
 	for (int i = 0; i < size; i++){
@@ -54,15 +71,25 @@ int transform_code_to_cpp(string program_location, string output){
 	}
 	
 	// functions
-	size = textsplit.size();
+	get_fuctions(textsplit);
+	size = custom_functions.size();
 	for (int i = 0; i < size; i++){
-		if (check_if_string_is_in_string(textsplit.at(i),"func ")){
-			if (check_if_string_is_in_string(textsplit.at(i),"func start")){
-			} else{
-				thecode.push_back("void");
-			}
-		}
+		string functext = custom_functions_type.at(i) + " " + custom_functions.at(i) + custom_functions_arg.at(i) + ";";
+		thecode.push_back(functext);
 	}
+	
+	// add main function
+	thecode.push_back("int main(){");
+	read_function(textsplit, "start");
+	
+	int start_end = when_function_ends(textsplit, func_start_position);
+	for (int i = func_start_position; i<=start_end ; i++){
+		thecode.push_back(ident(1) + textsplit.at(i));
+	}
+	
+	thecode.push_back(ident(1) + "cosas");
+	thecode.push_back("}");
+	
 	
 	// print
 	size = thecode.size();
@@ -71,10 +98,77 @@ int transform_code_to_cpp(string program_location, string output){
 	}
 	
 	return 0;
+}
+int when_function_ends(vector<string> textsplit, int function_position){
+	int size = textsplit.size();
+	int disparity = 0;
+	int end_position = 0;
+	bool changed = false;
+	for (int i = function_position; i < size; i++){
+		if (str_in(textsplit.at(i), " do")){
+			changed = true;
+			disparity++;
+		}
+		if (str_in(textsplit.at(i), "exit")){
+			changed = true;
+			disparity--;
+		}
+		if (str_in(textsplit.at(i), "else")){
+			changed = true;
+			disparity--;
+		}
+		if (disparity == 0 && changed == true){
+			end_position = i;
+			break;
+		}
+	}
+	return function_position + end_position - 1;
 	
 }
 
-bool check_if_string_is_in_string(string s1, string s2){
+void read_function(vector<string> textsplit, string function_name){
+	int functions_size = custom_functions.size();
+	if (function_name == "start"){
+		
+	} else{
+		for (int i = 0; i<functions_size; i++){
+			if (function_name == custom_functions.at(i)){
+				cout << "good" << function_name << endl;
+			}
+		}
+	}
+}
+
+void get_fuctions(vector<string> textsplit){
+	int size = textsplit.size();
+	for (int i = 0; i < size; i++){
+		if (str_in(textsplit.at(i),"func ")){
+			if (str_in(textsplit.at(i),"func start")){
+				//thecode.push_back("int main(){");
+				func_start_position = i;
+			} else {
+				//thecode.push_back("void");
+			}
+		} else {
+			if (str_in(textsplit.at(i),"func(")){
+				int variable_types_size = variable_types.size();
+				for (int j = 0; j<variable_types_size; j++){
+					if (str_in(textsplit.at(i),"func(" + variable_types.at(j) + ")")){
+						vector<string> thisline = split(textsplit.at(i), ' ');
+						custom_functions.push_back(thisline[1]);
+						custom_functions_type.push_back(variable_types.at(j));
+						custom_functions_arg.push_back("()");
+						int cfps = custom_functions_position.size();
+						custom_functions_position.push_back(cfps);
+					}
+				}
+			}
+		}
+	}
+	
+}
+
+bool str_in(string s1, string s2){
 	if(strstr(s1.c_str(),s2.c_str()))
 	{
 	   return true;
@@ -96,7 +190,7 @@ void write_text(string dir, string text){
 	cout << dir << " written" <<endl;
 }
 
-std::vector<std::string> split(const std::string &s, char delim) {
+vector<string> split(const std::string &s, char delim) {
   std::stringstream ss(s);
   std::string item;
   std::vector<std::string> elems;
@@ -105,4 +199,40 @@ std::vector<std::string> split(const std::string &s, char delim) {
     // elems.push_back(std::move(item)); // if C++11 (based on comment from @mchiasson)
   }
   return elems;
+}
+
+string ident(int num){
+	string nident = "	";
+	string tident;
+	for (int i = 0; i< num; i++){
+		tident = tident + nident;
+	}
+	return tident;
+}
+
+string rm_prefix(string full_string, string prefix){
+	string result = full_string.substr(prefix.length());
+	return result;
+}
+string rm_suffix(string full_string, string suffix){
+	string result = full_string.substr(0, full_string.length() - suffix.length());
+	return result;
+}
+
+bool str_starts_with(string full_string, string prefix){
+	if (full_string.rfind(prefix, 0) == 0) {
+		return true;
+	} else {
+		return false;
+	}
+} 
+
+vector<string> removeidentation (vector<string> textsplit){
+	int size = textsplit.size();
+	for (int i = 0; i<size; i++){
+		while (str_starts_with(textsplit.at(i), ident(1) ) ){
+			textsplit.at(i) = rm_prefix(textsplit.at(i), ident(1));
+		}
+	}
+	return textsplit;
 }
